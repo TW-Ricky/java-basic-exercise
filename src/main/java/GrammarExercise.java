974a89c7df609f3246f9ca0adec7cba9a02d2e57
gang yue <gang.yue@thoughtworks.com>
2020-07-29 11:45:59 +0800

    [yuegang] feat: finish the java-basic-exercise

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,39 @@
-import java.util.List;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GrammarExercise {
     public static void main(String[] args) {
-        //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
-
-        List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
-        //按要求输出到命令行
-
+        Scanner scanner = new Scanner(System.in);
+        String firstWordList = scanner.nextLine();
+        String secondWordList = scanner.nextLine();
+        try{
+            List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
+            System.out.println(result.toString());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
-        //在这编写实现代码
-        return null;
+        String[] firstString = firstWordList.split(",");
+        String[] secondString = secondWordList.split(",");
+
+        Stream<String> opstream = Stream.concat(Arrays.stream(firstString), Arrays.stream(secondString));
+        long cnt = opstream.filter(str -> "".equals(str) || !str.matches("[a-zA-Z]+")).count();
+        if (cnt != 0) {
+            throw new RuntimeException("input not valid");
+        }
+        Stream<String> firstStream = Arrays.stream(firstString);
+        List<String> secondUpperList = Arrays.stream(secondString).map(str2 -> str2.toUpperCase()).collect(Collectors.toList());
+        List<String> ans = firstStream.distinct()
+                .map(str -> str.toUpperCase())
+                .filter(str -> secondUpperList.contains(str))
+                .sorted().map(str -> String.join(" ", str.split("")))
+                .collect(Collectors.toList());
+
+        return ans;
     }
 }
